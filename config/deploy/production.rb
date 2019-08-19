0f39a1529d546792dc11b3f52449ae4452633975
Alex Dubov <adubov@cps.edu>
2019-08-19 09:56:48 -0500

    config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server "167.99.226.66", user: "deploy", roles: %w{app db web}, sudo: true
+server "167.99.226.66", user: "deploy", roles: %w{app db web}
 set :rails_env, 'production'
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/packs", ".bundle", "node_modules", '.bundle', 'credentials'
